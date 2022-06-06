f737e384b106351be034e1e3a8594ac322770006
shterneregen <shterneregen@gmail.com>
2022-06-06 23:49:05 +0300

    init commit with sdjpa-intro

---
+++ sdjpa-intro/src/main/java/random/sdjpaintro/SdjpaIntroApplication.java
@@ -0,0 +1,11 @@
+package random.sdjpaintro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdjpaIntroApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SdjpaIntroApplication.class, args);
+    }
+}
---
+++ sdjpa-intro/src/test/java/random/sdjpaintro/SdjpaIntroApplicationTests.java
@@ -0,0 +1,13 @@
+package random.sdjpaintro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SuppressWarnings("java:S2699")
+@SpringBootTest
+class SdjpaIntroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
