1d900f945198f8a776f1399a53d2a0411dec969f
shterneregen <shterneregen@gmail.com>
2022-06-07 00:04:23 +0300

    JPA entity + lombok

---
+++ sdjpa-intro/src/main/java/random/sdjpaintro/domain/Book.java
@@ -0,0 +1,28 @@
+package random.sdjpaintro.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String title;
+    private String isbn;
+    private String publisher;
+}
