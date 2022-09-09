6d19ebf4c2f68ced15c64fe5dce5c99e1e94e890
shterneregen <shterneregen@gmail.com>
2022-09-09 12:31:54 +0300

    Tests for UUID primary keys

--- sdjpa-flyway/src/test/java/random/sdjpaflyway/MySQLIntegrationTest.java
+++ sdjpa-flyway/src/test/java/random/sdjpaflyway/MySQLIntegrationTest.java
@@ -6,19 +6,47 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.context.ActiveProfiles;
+import random.sdjpaflyway.domain.AuthorUuid;
+import random.sdjpaflyway.domain.BookUuid;
+import random.sdjpaflyway.repositories.AuthorUuidRepository;
 import random.sdjpaflyway.repositories.BookRepository;
+import random.sdjpaflyway.repositories.BookUuidRepository;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @ActiveProfiles("local")
 @DataJpaTest
-@ComponentScan(basePackages = "random.sdjpaintro.bootstrap")
+@ComponentScan(basePackages = "random.sdjpaflyway.bootstrap")
 // By default, @DataJpaTest uses H2, here we say don't replace main db (MySQL) by H2
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class MySQLIntegrationTest {
 
     @Autowired
     private BookRepository bookRepository;
+    @Autowired
+    private AuthorUuidRepository authorUuidRepository;
+    @Autowired
+    private BookUuidRepository bookUuidRepository;
+
+    @Test
+    void testBookUuid() {
+        BookUuid bookUuid = bookUuidRepository.save(new BookUuid());
+        assertThat(bookUuid).isNotNull();
+        assertThat(bookUuid.getId()).isNotNull();
+
+        BookUuid fetched = bookUuidRepository.getReferenceById(bookUuid.getId());
+        assertThat(fetched).isNotNull();
+    }
+
+    @Test
+    void testAuthorUuid() {
+        AuthorUuid authorUuid = authorUuidRepository.save(new AuthorUuid());
+        assertThat(authorUuid).isNotNull();
+        assertThat(authorUuid.getId()).isNotNull();
+
+        AuthorUuid fetched = authorUuidRepository.getReferenceById(authorUuid.getId());
+        assertThat(fetched).isNotNull();
+    }
 
     @Test
     void testMySQL() {
