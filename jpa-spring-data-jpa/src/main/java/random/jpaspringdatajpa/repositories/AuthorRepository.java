68b8444e750eb22c4ce7b1408b9e56a7011a03f4
shterneregen <shterneregen@gmail.com>
2022-10-03 23:53:32 +0300

    Spring Data JPA Queries. Query method

--- jpa-spring-data-jpa/src/main/java/random/jpaspringdatajpa/repositories/AuthorRepository.java
+++ jpa-spring-data-jpa/src/main/java/random/jpaspringdatajpa/repositories/AuthorRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import random.jpaspringdatajpa.domain.Author;
 
 public interface AuthorRepository extends JpaRepository<Author, Long> {
+    Author findAuthorByFirstNameAndLastName(String firstName, String lastName);
 }
