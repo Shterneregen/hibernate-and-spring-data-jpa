91c7a5c7aa2e656d78eecdd544704f0f6408bf52
shterneregen <shterneregen@gmail.com>
2022-10-04 00:14:20 +0300

    Spring Data JPA Queries. Null handling

---
+++ jpa-spring-data-jpa/src/main/java/random/jpaspringdatajpa/repositories/package-info.java
@@ -0,0 +1,2 @@
+@org.springframework.lang.NonNullApi
+package random.jpaspringdatajpa.repositories;
---
+++ jpa-spring-data-jpa/src/test/java/random/jpaspringdatajpa/BookRepositoryTest.java
@@ -0,0 +1,39 @@
+package random.jpaspringdatajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.test.context.ActiveProfiles;
+import random.jpaspringdatajpa.repositories.BookRepository;
+
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@ActiveProfiles("local")
+@DataJpaTest
+@ComponentScan(basePackages = {"random.dao"})
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class BookRepositoryTest {
+
+    @Autowired
+    BookRepository bookRepository;
+
+    @Test
+    void testEmptyResultException() {
+
+        assertThrows(EmptyResultDataAccessException.class, () -> bookRepository.readByTitle("foobar4"));
+    }
+
+    @Test
+    void testNullParam() {
+        assertNull(bookRepository.getByTitle(null));
+    }
+
+    @Test
+    void testNoException() {
+        assertNull(bookRepository.getByTitle("foo"));
+    }
+}
