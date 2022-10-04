033e638d29bf9054ad2f1ef4099eda92d92f457d
shterneregen <shterneregen@gmail.com>
2022-10-05 00:49:24 +0300

    Paging and Sorting. Hibernate. Paging and Sorting

--- jpa-spring-data-jpa/src/test/java/random/jpaspringdatajpa/BookDaoHibernateTest.java
+++ jpa-spring-data-jpa/src/test/java/random/jpaspringdatajpa/BookDaoHibernateTest.java
@@ -7,6 +7,7 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.context.ActiveProfiles;
 import random.jpaspringdatajpa.dao.BookDao;
 import random.jpaspringdatajpa.dao.BookDaoHibernate;
@@ -36,6 +37,11 @@ class BookDaoHibernateTest {
 
     @Test
     void findAllBooksSortByTitle() {
+        List<Book> books = bookDao.findAllBooksSortByTitle(PageRequest.of(0, 10,
+                Sort.by(Sort.Order.desc("title"))));
+
+        assertThat(books).isNotNull();
+        assertThat(books.size()).isEqualTo(10);
     }
 
     @Test
