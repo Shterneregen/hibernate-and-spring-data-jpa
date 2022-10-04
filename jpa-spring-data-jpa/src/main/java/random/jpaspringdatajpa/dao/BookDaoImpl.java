334a30072cd127d21bdf54b0d52e317027b66404
shterneregen <shterneregen@gmail.com>
2022-10-05 01:21:34 +0300

    Paging and Sorting. Spring Data JPA. Paging and Sorting

--- jpa-spring-data-jpa/src/main/java/random/jpaspringdatajpa/dao/BookDaoImpl.java
+++ jpa-spring-data-jpa/src/main/java/random/jpaspringdatajpa/dao/BookDaoImpl.java
@@ -1,6 +1,8 @@
 package random.jpaspringdatajpa.dao;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,17 +20,27 @@ public class BookDaoImpl implements BookDao {
 
     @Override
     public List<Book> findAllBooksSortByTitle(Pageable pageable) {
-        return null;
+        Page<Book> bookPage = bookRepository.findAll(pageable);
+
+        return bookPage.getContent();
     }
 
     @Override
     public List<Book> findAllBooks(Pageable pageable) {
-        return null;
+        return bookRepository.findAll(pageable).getContent();
     }
 
     @Override
     public List<Book> findAllBooks(int pageSize, int offset) {
-        return null;
+        Pageable pageable = PageRequest.ofSize(pageSize);
+
+        if (offset > 0) {
+            pageable = pageable.withPage(offset / pageSize);
+        } else {
+            pageable = pageable.withPage(0);
+        }
+
+        return this.findAllBooks(pageable);
     }
 
     @Override
--- jpa-spring-data-jpa/src/main/java/random/jpaspringdatajpa/repositories/AuthorRepository.java
+++ jpa-spring-data-jpa/src/main/java/random/jpaspringdatajpa/repositories/AuthorRepository.java
@@ -1,5 +1,7 @@
 package random.jpaspringdatajpa.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import random.jpaspringdatajpa.domain.Author;
 
@@ -7,4 +9,6 @@ import java.util.Optional;
 
 public interface AuthorRepository extends JpaRepository<Author, Long> {
     Optional<Author> findAuthorByFirstNameAndLastName(String firstName, String lastName);
+
+    Page<Author> findAuthorByLastName(String lastName, Pageable pageable);
 }
