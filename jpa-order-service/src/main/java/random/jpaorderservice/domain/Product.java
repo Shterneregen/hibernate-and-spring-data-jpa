29615b4bbd1d22ccad7756041cd40bfed608be28
shterneregen <shterneregen@gmail.com>
2022-10-15 17:38:34 +0300

    Relationship Mappings. Many To Many

--- jpa-order-service/src/main/java/random/jpaorderservice/domain/Product.java
+++ jpa-order-service/src/main/java/random/jpaorderservice/domain/Product.java
@@ -1,18 +1,26 @@
 package random.jpaorderservice.domain;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
+import javax.persistence.*;
+import java.util.Set;
 
 @Getter
 @Setter
+@EqualsAndHashCode
 @Entity
 public class Product extends BaseEntity {
     private String description;
 
     @Enumerated(EnumType.STRING)
     private ProductStatus productStatus;
+
+    @EqualsAndHashCode.Exclude
+    @ManyToMany
+    @JoinTable(name = "product_category",
+            joinColumns = @JoinColumn(name = "product_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private Set<Category> categories;
 }
--- jpa-order-service/src/main/java/random/jpaorderservice/repositories/ProductRepository.java
+++ jpa-order-service/src/main/java/random/jpaorderservice/repositories/ProductRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import random.jpaorderservice.domain.Product;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    Product findByDescription(String description);
 }
