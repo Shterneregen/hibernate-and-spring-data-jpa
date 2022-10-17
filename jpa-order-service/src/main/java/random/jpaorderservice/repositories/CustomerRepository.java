40f4e32e9900b958e13b9fad5e256ad09cce2913
shterneregen <shterneregen@gmail.com>
2022-10-18 00:29:11 +0300

    Fetch Operations. Load test data

--- jpa-order-service/src/main/java/random/jpaorderservice/repositories/CustomerRepository.java
+++ jpa-order-service/src/main/java/random/jpaorderservice/repositories/CustomerRepository.java
@@ -3,5 +3,8 @@ package random.jpaorderservice.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import random.jpaorderservice.domain.Customer;
 
+import java.util.Optional;
+
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    Optional<Customer> findCustomerByCustomerNameIgnoreCase(String customerName);
 }
--- jpa-order-service/src/main/java/random/jpaorderservice/repositories/ProductRepository.java
+++ jpa-order-service/src/main/java/random/jpaorderservice/repositories/ProductRepository.java
@@ -3,6 +3,8 @@ package random.jpaorderservice.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import random.jpaorderservice.domain.Product;
 
+import java.util.Optional;
+
 public interface ProductRepository extends JpaRepository<Product, Long> {
-    Product findByDescription(String description);
+    Optional<Product> findByDescription(String description);
 }
