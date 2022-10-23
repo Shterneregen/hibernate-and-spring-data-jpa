b7d8fdbbf1b1a6666698be993f222788070de1ad
shterneregen <shterneregen@gmail.com>
2022-10-23 22:56:18 +0300

    Adding Validation

--- jpa-order-service/src/main/java/random/jpaorderservice/domain/Address.java
+++ jpa-order-service/src/main/java/random/jpaorderservice/domain/Address.java
@@ -3,12 +3,17 @@ package random.jpaorderservice.domain;
 import lombok.Data;
 
 import javax.persistence.Embeddable;
+import javax.validation.constraints.Size;
 
 @Data
 @Embeddable
 public class Address {
+    @Size(max = 30)
     private String address;
+    @Size(max = 30)
     private String city;
+    @Size(max = 30)
     private String state;
+    @Size(max = 30)
     private String zipCode;
 }
--- jpa-order-service/src/main/java/random/jpaorderservice/domain/Customer.java
+++ jpa-order-service/src/main/java/random/jpaorderservice/domain/Customer.java
@@ -7,6 +7,9 @@ import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 import javax.persistence.Version;
+import javax.validation.Valid;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Size;
 import java.util.LinkedHashSet;
 import java.util.Set;
 
@@ -14,13 +17,17 @@ import java.util.Set;
 @Setter
 @Entity
 public class Customer extends BaseEntity {
-
+    @Size(max = 50)
     private String customerName;
 
+    @Valid
     @Embedded
     private Address address;
 
+    @Size(max = 20)
     private String phone;
+    @Size(max = 255)
+    @Email
     private String email;
     @Version
     private Integer version;
