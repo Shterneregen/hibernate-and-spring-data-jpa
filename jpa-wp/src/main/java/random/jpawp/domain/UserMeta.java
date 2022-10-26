b7a4d38d92e1614386f68dd9e3265e52dd7c0123
shterneregen <shterneregen@gmail.com>
2022-10-27 00:29:27 +0300

    WP. Adding validation

--- jpa-wp/src/main/java/random/jpawp/domain/UserMeta.java
+++ jpa-wp/src/main/java/random/jpawp/domain/UserMeta.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.Size;
 
 @Getter
 @Setter
@@ -16,6 +17,7 @@ public class UserMeta {
     @Column(name = "umeta_id")
     private Long id;
     private Long userId;
+    @Size(max = 255)
     private String metaKey;
 
     @Lob
