b2c7363f62acc2fd68cefed6f7800a4c93dd7562
shterneregen <shterneregen@gmail.com>
2022-10-29 19:18:07 +0300

    Credit Card Project. Hibernate interceptor

--- jpa-credit-card/src/test/java/random/jpacreditcard/repositories/CreditCardRepositoryTest.java
+++ jpa-credit-card/src/test/java/random/jpacreditcard/repositories/CreditCardRepositoryTest.java
@@ -4,9 +4,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ActiveProfiles;
 import random.jpacreditcard.domain.CreditCard;
 import random.jpacreditcard.services.EncryptionService;
+
+import java.util.Map;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
@@ -21,6 +24,8 @@ class CreditCardRepositoryTest {
     private CreditCardRepository creditCardRepository;
     @Autowired
     private EncryptionService encryptionService;
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
     @Test
     void testSaveAndStoreCreditCard() {
@@ -31,10 +36,18 @@ class CreditCardRepositoryTest {
 
         CreditCard savedCC = creditCardRepository.saveAndFlush(creditCard);
 
-        System.out.println("Getting CC from database: " + creditCard.getCreditCardNumber());
+        System.out.println("Getting CC from database: " + savedCC.getCreditCardNumber());
 
         System.out.println("CC At Rest");
         System.out.println("CC Encrypted: " + encryptionService.encrypt(CREDIT_CARD));
+
+        Map<String, Object> dbRow = jdbcTemplate.queryForMap("SELECT * FROM credit_card " +
+                "WHERE id = " + savedCC.getId());
+
+        String dbCardValue = (String) dbRow.get("credit_card_number");
+
+        assertThat(savedCC.getCreditCardNumber()).isNotEqualTo(dbCardValue);
+        assertThat(dbCardValue).isEqualTo(encryptionService.encrypt(CREDIT_CARD));
 
         CreditCard fetchedCC = creditCardRepository.findById(savedCC.getId()).get();
 
