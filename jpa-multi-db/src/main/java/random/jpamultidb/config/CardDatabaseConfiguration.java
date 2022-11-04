1f5f765e46b9176d334916b0ca18578d4bc5ccd6
shterneregen <shterneregen@gmail.com>
2022-11-04 15:54:59 +0300

    Multi DB Project. Multiple DB config

---
+++ jpa-multi-db/src/main/java/random/jpamultidb/config/CardDatabaseConfiguration.java
@@ -0,0 +1,54 @@
+package random.jpamultidb.config;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+import random.jpamultidb.domain.creditcard.CreditCard;
+
+import javax.sql.DataSource;
+
+@EnableJpaRepositories(
+        basePackages = "random.jpamultidb.repositories.creditcard",
+        entityManagerFactoryRef = "cardEntityManagerFactory",
+        transactionManagerRef = "cardTransactionManager"
+)
+@Configuration
+public class CardDatabaseConfiguration {
+    @Bean
+    @ConfigurationProperties("spring.card.datasource")
+    public DataSourceProperties cardDataSourceProperties() {
+        return new DataSourceProperties();
+    }
+
+    @Bean
+    public DataSource cardDataSource(
+            @Qualifier("cardDataSourceProperties") DataSourceProperties cardDataSourceProperties) {
+        return cardDataSourceProperties.initializeDataSourceBuilder()
+                .type(HikariDataSource.class)
+                .build();
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean cardEntityManagerFactory(
+            @Qualifier("cardDataSource") DataSource cardDataSource,
+            EntityManagerFactoryBuilder builder) {
+        return builder.dataSource(cardDataSource)
+                .packages(CreditCard.class)
+                .persistenceUnit("card")
+                .build();
+    }
+
+    @Bean
+    public PlatformTransactionManager cardTransactionManager(
+            @Qualifier("cardEntityManagerFactory") LocalContainerEntityManagerFactoryBean cardEntityManagerFactory) {
+        return new JpaTransactionManager(cardEntityManagerFactory.getObject());
+    }
+}
---
+++ jpa-multi-db/src/main/java/random/jpamultidb/config/PanDatabaseConfiguration.java
@@ -0,0 +1,59 @@
+package random.jpamultidb.config;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+import random.jpamultidb.domain.pan.CreditCardPAN;
+
+import javax.sql.DataSource;
+
+@EnableJpaRepositories(
+        basePackages = "random.jpamultidb.repositories.pan",
+        entityManagerFactoryRef = "panEntityManagerFactory",
+        transactionManagerRef = "panTransactionManager"
+)
+@Configuration
+public class PanDatabaseConfiguration {
+    @Bean
+    @Primary
+    @ConfigurationProperties("spring.pan.datasource")
+    public DataSourceProperties panDataSourceProperties() {
+        return new DataSourceProperties();
+    }
+
+    @Primary
+    @Bean
+    public DataSource panDataSource(
+            @Qualifier("panDataSourceProperties") DataSourceProperties panDataSourceProperties) {
+        return panDataSourceProperties.initializeDataSourceBuilder()
+                .type(HikariDataSource.class)
+                .build();
+    }
+
+    @Primary
+    @Bean
+    public LocalContainerEntityManagerFactoryBean panEntityManagerFactory(
+            @Qualifier("panDataSource") DataSource panDataSource,
+            EntityManagerFactoryBuilder builder) {
+        return builder.dataSource(panDataSource)
+                .packages(CreditCardPAN.class)
+                .persistenceUnit("pan")
+                .build();
+    }
+
+    @Primary
+    @Bean
+    public PlatformTransactionManager panTransactionManager(
+            @Qualifier("panEntityManagerFactory") LocalContainerEntityManagerFactoryBean panEntityManagerFactory) {
+        return new JpaTransactionManager(panEntityManagerFactory.getObject());
+    }
+}
